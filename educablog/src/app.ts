7876ebe962c826f52f28701405a8d9c55cab9264
RichardKT88 <kendy_t88@hotmail.com>
2026-06-13 11:17:15 -0300

    implementa swagger para documentacao de api

--- educablog/src/app.ts
+++ educablog/src/app.ts
@@ -1,5 +1,9 @@
 import express from 'express'
+import swaggerUi from 'swagger-ui-express'           
+import { swaggerDocument } from './docs/swagger'
 
 export const app = express()
 
 app.use(express.json())
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))  // + adicionar
